204bcc304ce29454174960ffe1f9bb00879412c8
gorkemtand <tandogangorkem@gmail.com>
2023-06-01 19:55:43 +0300

    New Updates

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,7 @@ function Input(props) {
   return (
     <div className={"inputWrapper"}>
       {props.icon && <div className={"icon"}>{props.icon}</div>}
-      <input type={props.text} placeholder={props.placeholder} />
+      <input type={props.type} placeholder={props.placeholder} />
     </div>
   );
 }
